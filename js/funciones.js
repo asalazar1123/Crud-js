61b621558b575ac2ec4c01778cf7875a64fe25a6
asalazar1123 <salazaralexander2@gmail.com>
2023-08-07 22:47:44 -0400

    funcion alertas

--- js/funciones.js
+++ js/funciones.js
@@ -5,12 +5,12 @@ let rowSeleccionada = null; //Esta variable se usará más adelante para almacen
 
 // mostramos alertas
 //todo: se define funcion que muestra alerta que acepta los parametros mensaje y nombreEmpresa con lo cual crearemos alertas en la interfaz
-function mostrarAlerta(mensaje, className){
+function mostrarAlerta(message, className){
     const div = document.createElement("div");
-    div.className = 'alert alert-${className}';
+    div.className = `alert alert-${className}`;
     
-    div.appendChild(document.createTextNode(mensaje)); // Esto coloca el mensaje dentro de la alerta.
-    const container = document.querySelector(".container");//aqui seleccionamos los elementos del DOM
+    div.appendChild(document.createTextNode(message)); // Esto coloca el mensaje dentro de la alerta.
+    const container = document.querySelectorAll(".container")[1];//aqui seleccionamos los elementos del DOM
     const main = document.querySelector(".main");//aqui seleccionamos los elementos del DOM
     container.insertBefore(div, main);//aqui seleccionamos los elementos del DOM
 
@@ -29,7 +29,35 @@ function borrarCampos(){
 
 //AÑADIR DATOS
 
+document.querySelector("#registro").addEventListener("submit", (e)=>{
+    e.preventDefault();
 
+    //obtenemos valor de form
+    const nombreEmpresa = document.querySelector("#empresa").value;
+
+    //validamos
+    debugger
+    if( empresa.value == ""){
+         mostrarAlerta("Porfavor ingresa el nombre de tu empresa para continuar" , "danger");
+    }
+    else{
+        if(rowSeleccionada == null){
+            const lista = document.querySelector("#registro");
+            const row = document.createComment("tr");
+
+            row.innerHTML = `
+            <td>${nombreEmpresa}</td>
+            <td>
+            <a href="#" class="btn btn-warning btn-sm editar">Editar</a> 
+            <a href="#" class="btn btn-danger btn-sm borrar">Eliminar</a>`;
+
+            lista.appendChild(row);
+            rowSeleccionada = null;
+            mostrarAlerta("Empresa agregada", "succes");
+        }
+    }
+
+});
 
 
 // BORRAMOS DATA
